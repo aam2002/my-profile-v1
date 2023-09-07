3e90e8b0ec494b19d27120812673f3d9cfa32e9c
aman <amansoni20020@gmail.com>
2023-09-07 21:01:56 +0530

    checked in laptop

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,15 +8,6 @@ const Nav = () => {
       <div className="w-full h-7 flex justify-between  p-9 fixed z-10 nav">
         <div className="flex ">
           <h1 className=" text-2xl px-8  font-light text-white hover:text-purple-300 ">
-{/* <Button >
-
-
-<MenuIcon>
-<input type="checkbox" id="check" className=""/>
-<label htmlFor="check">{document.getElementById("check")?console.log("good"):console.log('bad')}</label>   
-</MenuIcon> 
-</Button> */}
-
             <a href=".">.Soni</a>
           </h1>
         </div>
@@ -42,11 +33,8 @@ const Nav = () => {
           <h1 className="px-8  font-light text-purple-400 hover:text-pink-500 ">
             <a href="#Contact">CONTACT</a>
           </h1>
-         
         </div>
-
       </div>
-      
     </nav>
   );
 };
